eead60e7690a5fe3d55fb80c6e032f69a8ffe9fe
Arthur C. Philippe <arthur.philippe@epitech.eu>
2017-04-18 11:23:18 +0200

    chess board integration on planes

--- src/procedural_texturing/chest_board.c
+++ src/procedural_texturing/chest_board.c
@@ -5,16 +5,16 @@
 ** Login   <etienne.filliat@epitech.eu>
 **
 ** Started on  Tue Apr 11 17:26:49 2017 etienne.filliat
-** Last update Tue Apr 11 17:49:21 2017 etienne.filliat
+** Last update Tue Apr 18 11:20:51 2017 Arthur Philippe
 */
 
 #include <SFML/Graphics/RenderWindow.h>
 #include <SFML/Graphics/Sprite.h>
 #include <SFML/Graphics/Texture.h>
 #include "raytracer.h"
-#define D	300
+#define D	50
 
-static void		positiv_positiv(sfVector2i pos, sfColor *color)
+static void		positive_positive(sfVector2i pos, sfColor *color)
 {
   if (((pos.x > (D / 2) && pos.y > (D / 2))
        || (pos.x <= (D / 2) && pos.y <= (D / 2))))
@@ -23,7 +23,7 @@ static void		positiv_positiv(sfVector2i pos, sfColor *color)
     *color = sfBlack;
 }
 
-static void		negativ_negativ(sfVector2i pos, sfColor *color)
+static void		negative_negative(sfVector2i pos, sfColor *color)
 {
   if ((pos.x > (D / 2 * -1) && pos.y > (D / 2 * -1))
       || (pos.x <= (D / 2 * -1) && pos.y <= (D / 2 * -1)))
@@ -32,7 +32,7 @@ static void		negativ_negativ(sfVector2i pos, sfColor *color)
     *color = sfBlack;
 }
 
-static void		positiv_negativ(sfVector2i pos, sfColor *color)
+static void		positive_negative(sfVector2i pos, sfColor *color)
 {
   if ((pos.x > (D / 2) && pos.y > (D / 2 * -1))
       || (pos.x <= (D / 2) && pos.y <= (D / 2 * -1)))
@@ -41,7 +41,7 @@ static void		positiv_negativ(sfVector2i pos, sfColor *color)
     *color = sfBlack;
 }
 
-static void		negativ_positiv(sfVector2i pos, sfColor *color)
+static void		negative_positive(sfVector2i pos, sfColor *color)
 {
   if ((pos.x > (D / 2 * -1) && pos.y > (D / 2))
       || (pos.x <= (D / 2 * -1) && pos.y <= (D / 2)))
@@ -58,11 +58,11 @@ void			get_damier_color(sfVector3f last_intersect,
   pos.x = (int) last_intersect.x % D;
   pos.y = (int) last_intersect.y % D;
   if (pos.x >= 0 && pos.y >= 0)
-    positiv_positiv(pos, color);
+    positive_positive(pos, color);
   else if (pos.x < 0 && pos.y < 0)
-    negativ_negativ(pos, color);
+    negative_negative(pos, color);
   else if (pos.x >= 0 && pos.y < 0)
-    positiv_negativ(pos, color);
+    positive_negative(pos, color);
   else
-    negativ_positiv(pos, color);
+    negative_positive(pos, color);
 }
--- src/render/raytrace.c
+++ src/render/raytrace.c
@@ -5,7 +5,7 @@
 ** Login   <arthur.philippe@epitech.eu>
 **
 ** Started on  Sat Apr 15 13:26:22 2017 Arthur Philippe
-** Last update Mon Apr 17 22:09:08 2017 Arthur Philippe
+** Last update Tue Apr 18 11:20:29 2017 Arthur Philippe
 */
 
 #include <SFML/Graphics/RenderWindow.h>
@@ -88,6 +88,8 @@ void		raytrace_full_scene(t_env *env)
       if (out.k > 0)
 	{
 	  px.color = get_def_color_ty(out.type);
+	  if (out.type == ID_PLANE)
+	    get_damier_color(out.hit_pt, &(px.color));
 	  px.color.a *= std_color_effect(env, &out);
 	  my_put_pixel(env->w->buffer, px.pos.x - 1, px.pos.y, px.color);
 	}
