b04ed210edc5a2019470c6dba5ac0fec8ad63c47
mael.drapier <mael.drapier@epitech.eu>
2017-05-03 16:02:36 +0200

    removed*

--- src/render/set_brightness.c
+++ src/render/set_brightness.c
@@ -5,7 +5,7 @@
 ** Login   <mael.drapier@epitech.eu>
 **
 ** Started on  Tue May  2 15:24:50 2017 mael drapier
-** Last update Wed May  3 15:59:58 2017 mael drapier
+** Last update Wed May  3 16:02:24 2017 mael drapier
 */
 
 #include <SFML/Graphics/RenderWindow.h>
@@ -17,7 +17,7 @@ void	set_brightness(sfColor *color, t_object *objects, float coef)
 {
   while (objects)
     {
-      if (objects->type == ID_LIGHT && objects->col.a)
+      if (objects->type == ID_LIGHT &&  objects->col.a)
 	{
 	  color->r += 0.5 * objects->col.r * coef;
 	  color->g += 0.5 * objects->col.g * coef;
