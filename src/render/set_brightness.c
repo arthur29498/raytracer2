204a5d895d8274dd88e7301c38146e14e776bfbd
mael.drapier <mael.drapier@epitech.eu>
2017-05-03 14:22:39 +0200

    initializing set_brightness after apply_colored_light_effect

--- src/render/set_brightness.c
+++ src/render/set_brightness.c
@@ -5,5 +5,15 @@
 ** Login   <mael.drapier@epitech.eu>
 **
 ** Started on  Tue May  2 15:24:50 2017 mael drapier
-** Last update Tue May  2 15:24:58 2017 mael drapier
+** Last update Wed May  3 14:20:37 2017 mael drapier
 */
+
+#include <SFML/Graphics/RenderWindow.h>
+#include <SFML/Graphics/Sprite.h>
+#include <SFML/Graphics/Texture.h>
+#include "raytracer.h"
+
+void	set_brightness(sfColor *color, t_object *objects)
+{
+
+}
