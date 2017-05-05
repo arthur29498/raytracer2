522995e7c7c585bec70c83e79d679f8d4796872d
Arthur C. Philippe <arthur.philippe@epitech.eu>
2017-05-05 19:02:21 +0200

    I don't get it

--- src/vectors/reflect.c
+++ src/vectors/reflect.c
@@ -5,14 +5,17 @@
 ** Login   <arthur.philippe@epitech.eu>
 **
 ** Started on  Thu May  4 10:33:17 2017 Arthur Philippe
-** Last update Fri May  5 11:45:24 2017 Arthur Philippe
+** Last update Fri May  5 18:55:59 2017 Arthur Philippe
 */
 
 #include <SFML/Graphics/RenderWindow.h>
 #include <SFML/Graphics/Sprite.h>
 #include <SFML/Graphics/Texture.h>
+#include <stdio.h>
 #include "math.h"
 #include "raytracer.h"
+
+// rd = 2 * dot(n,id) * n - id
 
 static sfVector3f	reflect_vector(sfVector3f dir_v,
 				       sfVector3f normal_v)
@@ -22,17 +25,21 @@ static sfVector3f	reflect_vector(sfVector3f dir_v,
 
   dir_v = normalize_vector(dir_v);
   normal_v = normalize_vector(normal_v);
-  dot = dot_product(dir_v, normal_v);
-  reflected_v.x = (normal_v.x * -2 * dot) + dir_v.x;
-  reflected_v.y = (normal_v.y * -2 * dot) + dir_v.y;
-  reflected_v.z = (normal_v.z * -2 * dot) + dir_v.z;
+  dot = dot_product(normal_v, dir_v);
+  reflected_v.x = -2 * dot * normal_v.x - dir_v.x;
+  reflected_v.y = -2 * dot * normal_v.y - dir_v.y;
+  reflected_v.z = -2 * dot * normal_v.z - dir_v.z;
   return (reflected_v);
 }
 
 static void	prep_ray(t_render_in *in,
 			 t_render_out *pr_out)
 {
-  in->dir_vector = reflect_vector(pr_out->last_dir_v, pr_out->normal);
+  sfVector3f	reflected_v;
+
+  reflected_v = reflect_vector(pr_out->last_dir_v, pr_out->normal);
+  // printf("%f %f %f\n%f %f %f\n--------------\n", reflected_v.x, reflected_v.y, reflected_v.y, pr_out->normal.x, pr_out->normal.y, pr_out->normal.z);
+  in->dir_vector = reflected_v;
   in->eye_pt = pr_out->hit_pt;
   in->skip = pr_out->last_obj;
 }
@@ -44,6 +51,7 @@ void		reflect_effect(t_env *env,
   t_render_in	in;
   t_render_out	nw_out;
   sfColor	new_color;
+  float		coef;
 
   my_memset(&in, 0, sizeof(t_render_in));
   prep_ray(&in, pr_out);
@@ -51,6 +59,10 @@ void		reflect_effect(t_env *env,
   if (nw_out.k > 0)
     {
       new_color = get_color_from_objs(env->objects, nw_out.last_obj);
-      *color = reflect_color(*color, new_color, 0.4);
+      if (nw_out.type == ID_PLANE)
+	set_chessboard_color(nw_out.hit_pt, &new_color);
+      coef = std_color_effect(env, &nw_out);
+      new_color.a *= coef;
+      *color = reflect_color(*color, new_color, 0.3);
     }
 }
